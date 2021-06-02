796a4ff7d86d2e5ee49e958a8f78221e5417e57f
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-06-02 20:55:29 -0300

    queryURL

--- src/Messages.js
+++ src/Messages.js
@@ -30,8 +30,8 @@ const Messages = {
     div.innerHTML = content + div.innerHTML;
 
     div.style.background = `#${userPreferences.background}` || "bg-black"
-    cardMessages.style.background = `#${userPreferences.messageBackground}` || "bg-gray-800"
-    cardMessages.style.color = `#${userPreferences.textColor}` || "text-white"
+    cardMessages?.style.background = `#${userPreferences.messageBackground}` || "bg-gray-800"
+    cardMessages?.style.color = `#${userPreferences.textColor}` || "text-white"
   },
   channelClasses(name) {
     const channels = {
