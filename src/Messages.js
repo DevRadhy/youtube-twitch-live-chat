d8b804e647cb07706693b3c3353890e368d183b1
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-06-02 23:58:07 -0300

    Adicionando preferencias as mensagens

--- src/Messages.js
+++ src/Messages.js
@@ -1,4 +1,5 @@
 import { Storage } from './Storage.js';
+import { Preferences } from './Preferences';
 import { parseEmotes } from './Emotes.js';
 
 const Messages = {
@@ -11,18 +12,28 @@ const Messages = {
   },
   render({ tags, message, channel }) {
     const div = document.querySelector('div')
+    const body = document.querySelector('body')
     
     const channelClasses = Messages.channelClasses(channel).join(" ")
+
+    const userPreferences = Preferences.getAll()
 
     message = parseEmotes(message, tags)
     
     const content = `
-    <p class="mb-4 p-4 shadow bg-gray-800 text-white">
+    <p class="mb-4 p-4 shadow bg-gray-800 text-white" id="card-message">
       <span class="text-gray-400 text-sm uppercase mr-4 block tracking-wide ${channelClasses}">${channel}</span>
       <span style="color: ${tags.color}"> ${tags['display-name']}</span>: ${message} 
     </p>`
     
     div.innerHTML = content + div.innerHTML;
+
+    body.style.background = userPreferences.background
+
+    const cardMessage = document.getElementById('card-message')
+
+    cardMessage.style.background = userPreferences.messageColor
+    cardMessage.style.color = userPreferences.textColor
   },
   channelClasses(name) {
     const channels = {
